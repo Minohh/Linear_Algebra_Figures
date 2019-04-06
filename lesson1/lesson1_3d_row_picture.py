d7571a0ff29896f089b07499f8f457e7a3d32af4
Minohh <slswxom@gmail.com>
2019-04-06 09:17:51 -0700

    add edge grid for 3d scope box

--- lesson1/lesson1_3d_row_picture.py
+++ lesson1/lesson1_3d_row_picture.py
@@ -1,9 +1,6 @@
 # generate data
 import os
 from mayavi import mlab
-from mayavi.modules.api import GridPlane
-from mayavi.core.engine import Engine
-from mayavi.tools.show import show
 import numpy as np
 
 limit = 5
@@ -34,6 +31,22 @@ from sys import version_info
 if version_info[0] < 3:
     import pyface.qt
 
+def xyz_edge_grid(xstart, xend, ystart, yend, zstart, zend):
+    w = 11
+    h = 11
+    X1, Y1 = np.mgrid[xstart:xend:w*1j, ystart:yend:h*1j]
+    Z1 = np.full((w, h), zstart)
+    
+    Y2, Z2 = np.mgrid[ystart:yend:w*1j, zstart:zend:h*1j]
+    X2 = np.full((w, h), xstart)
+    
+    X3, Z3 = np.mgrid[xstart:xend:w*1j, zstart:zend:h*1j]
+    Y3 = np.full((w, h), ystart)
+
+    mlab.mesh(X1, Y1, Z1, representation='wireframe')
+    mlab.mesh(X2, Y2, Z2, representation='wireframe')
+    mlab.mesh(X3, Y3, Z3, representation='wireframe')
+
 def crop_xyz(x, y, z, limit):
     x[x>limit] = np.nan
     x[x<-limit] = np.nan
@@ -42,8 +55,9 @@ def crop_xyz(x, y, z, limit):
     z[z>limit] = np.nan
     z[z<-limit] = np.nan
 
-def gen_surfaces(e):
-#    fig = mlab.figure(size=(800, 800))
+def gen_surfaces():
+    mlab.figure(size=(800, 800))
+    xyz_edge_grid(-limit, limit, -limit, limit, -limit, limit)
 
     ax_ranges = [-limit, limit, -limit, limit, -limit, limit]
     ax_scale = [1.0, 1.0, 1.0]
@@ -55,63 +69,15 @@ def gen_surfaces(e):
     surf1 = mlab.mesh(X1, Y1, Z1, colormap='Blues')
     surf2 = mlab.mesh(X2, Y2, Z2, colormap='Oranges')
     surf3 = mlab.mesh(X3, Y3, Z3, colormap='black-white')
-#    v  = mlab.view(-30, 75, 8*limit, [0, 0, 0])
-    ol = mlab.outline(surf3, color=(.7, .7, .7), extent=ax_extent)
-    ax = mlab.axes(surf3, color=(.7, .7, .7), extent=ax_extent,
-              ranges=ax_ranges,
-              xlabel='x', ylabel='y', zlabel='z')
+    mlab.view(-30, 75, 8*limit, [0, 0, 0])
+    mlab.outline(surf3, color=(.7, .7, .7), extent=ax_extent)
+    #mlab.axes(surf3, color=(.7, .7, .7), extent=ax_extent,
+    #          ranges=ax_ranges,
+    #          xlabel='x', ylabel='y', zlabel='z')
 
-#    if transparency:
-#        surf3.actor.property.opacity = 0.5
-#        surf4.actor.property.opacity = 0.5
-#        fig.scene.renderer.use_depth_peeling = 1
-    e.add_module(surf1)
-    e.add_module(surf2)
-    e.add_module(surf3)
-    
-#    e.add_module(v)
-    e.add_module(ol)
-    e.add_module(ax)
 
-def gen_grid_plane(e):
-    gp1 = GridPlane()
-    e.add_module(gp1)
-    
-    gp2 = GridPlane()
-    gp2.grid_plane.axis = 'y'
-    gp2.grid_plane.position = 1
-    e.add_module(gp2)
+gen_surfaces()
 
-    gp3 = GridPlane()
-    gp3.grid_plane.axis = 'z'
-    gp3.grid_plane.position = limit
-    e.add_module(gp3)
+mlab.savefig(os.path.basename(__file__).replace(".py", "")+".png")
+mlab.show()
 
-e = Engine()
-e.start()
-e.new_scene()
-
-gen_surfaces(e)
-gen_grid_plane(e)
-
-e.current_scene.render()
-show()
-e.stop()
-
-#mlab.savefig(os.path.basename(__file__).replace(".py", "")+".png")
-
-#mlab.show()
-#v2_mayavi(True)
-
-# To install mayavi, the following currently works for me (Windows 10):
-#
-#   conda create --name mayavi_test_py2 python=2.7 matplotlib mayavi=4.4.0
-#    (installs pyqt=4.10.4 mayavi=4.4.0 vtk=5.10.1)
-#    * the `use_depth_peeling=1` got no effect. Transparency is not correct.
-#    * requires `import pyface.qt` or similar workaround
-#
-# or
-#
-#   conda create --name mayavi_test_py3 python=3.6 matplotlib
-#   conda activate mayavi_test_py3
-#   pip install mayavi
